5cd2dbb911833275a928485e8e44e5d06a920179
naeem <naeem.bazzazeh@gmail.com>
2024-05-17 12:18:53 +0200

    Initial commit

---
+++ LottoSpielSimulator/src/main/java/com/LottoSpielSimulator/LottoSpielSimulator/LottoSpielSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.LottoSpielSimulator.LottoSpielSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoSpielSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottoSpielSimulatorApplication.class, args);
+	}
+
+}
